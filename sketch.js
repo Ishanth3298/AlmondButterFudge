532b5c2b2bf11cd3949ac3b4c7e8c377bffb2571
Ishanth3298 <75448742+Ishanth3298@users.noreply.github.com>
2021-01-31 13:53:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+//1/4 cups peanut butter
+//1/4 cups Maple syrup
+//1 cup Out flour
+//1tsp cacoa powder
+//1tsp. Chocolate Chips
+//Heat syrup for 30secs in microwave
+//Mix syrup and add penut butter
+//Heat for 30sec.
+//Mix
+//Add all other ingredents
+//Mix until dough constincicy
+//Spread out on parchment paper
+//Freeze for 30 minutes
+//Enjoy (Store in freezer)
